62cc654ef578a1efa757459e978987a9e52ed65e
ad-6 <80580304+ad-6@users.noreply.github.com>
2021-04-15 10:22:32 +0530

    Add files via upload

---
+++ 3_Implementation/bank_main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "bankhead.h"
+
+/**
+ * @brief this function is used for calling all the other functions required for this system
+ * 
+ * @return int  null 
+ */
+int main()
+{
+    menu();
+    return 0;
+}
