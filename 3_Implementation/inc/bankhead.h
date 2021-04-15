613e9895aeb74ee7aaaec9d04e843f86affb067c
ad-6 <80580304+ad-6@users.noreply.github.com>
2021-04-15 19:40:26 +0530

    Update bankhead.h

--- 3_Implementation/inc/bankhead.h
+++ 3_Implementation/inc/bankhead.h
@@ -3,49 +3,43 @@
 
 int i,j;
 int main_exit;
-void menu();
-
-/**
- * @brief the structure date holds all the date that would be required like date of creation of account and date of birth of account holder
- * 
- */
-
-struct date{
-    int mnth,day,y;
-
-    };
 
 /**
  * @brief this structure holds all the vital components which are neccesary for the account creation/viewing/editing etc
  * 
  */
 
-struct {
-
+typedef struct acc_info
+{
     char name[60];
     int acc_no,age;
     char address[60];
-    double phone;
+    int phone;
     char acc_type[10];
     float amt;
-    struct date dob;
-    struct date dep;
-    
-    }add,upd,chk,del;
+}acc_info;
+
+/**
+ * @brief this is a for error 
+ * 
+ */
+
+typedef enum error_t
+{
+    ERROR_NULL_PTR = 0,    
+    SUCCESS = 1,            
+    ERROR_MEMORY_FULL = -1   
+}error_t;
 
 /**
  * @brief here all the functions that are being used in this program
  * 
  */
 
-    void menu(void);
-    float interest(float t,float a,int r);
-    void new_acc();
-    void view_all();
-    void edit(void);
-    void delete_acc(void);
-    void view_acc(void);
-    void exit_menu(void);
+void menu(acc_info *account, int *numberOfAccounts, char *filename);
+error_t new_acc(acc_info *account, int* numberOfAccounts);
+error_t view_acc(acc_info *account, int *numberOfAccounts);
+error_t write_func(acc_info *account, int *numberOfAccounts, char *filename);
 
 
 #endif  
