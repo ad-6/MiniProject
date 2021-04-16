7c571105d8c7f0d2ce74ee73613cc8cd5d9750cd
ad-6 <80580304+ad-6@users.noreply.github.com>
2021-04-16 11:06:30 +0530

    Update bank_check_acc.c

--- 3_Implementation/src/bank_check_acc.c
+++ 3_Implementation/src/bank_check_acc.c
@@ -9,50 +9,46 @@
  * 
  */
 
-error_t view_acc(acc_info *account, int *numberOfAccounts)
+error_t view_acc(acc_info *acc, int *num_acc)
 {
-    int choice, i, accountNo;
-    char name[30];
-    int searchflag = 0;
-    printf("Do you want to check by\n1.Account no\n2.Name\nEnter your choice:");
+    int choice, i, acc_choice,flag=0;
+    char name[60];
+    printf("Do you want to check by\n1.Account number \n2.Name\nEnter your choice:");
     scanf("%d", &choice);
     if (choice == 1)
     {
         printf("Enter the Account Number:\n");
-        scanf("%d", &accountNo);
-        for (i = 0; i < *numberOfAccounts; i++)
+        scanf("%d", &acc_choice);
+        for (i = 0; i < *num_acc; i++)
         {
-            if (account[i].account_no == accountNo)
+            if (acc[i].acc_no == acc_choice)
             {
-                printf("Given Account No. is registered with following details\n");
-                printf("Account No.:%d\tName:%s\tBalance:%f\tAccount Type:%s\n", account[i].account_no, account[i].name, account[i].balance, account[i].account_type);
-                searchflag = 1;
+                printf("\n The details are as follows");
+                printf("\n\tAccount No.:%d\n\tName:%s\n\tAmount:%f\n\tAccount Type:%s", acc[i].acc_no, acc[i].name, acc[i].amt, acc[i].acc_type);
+                flag = 1;
                 return SUCCESS;
             }
         }
-        if (searchflag == 0)
-        {
-            printf("Error. No record found.\n");
-        }
+        if (flag == 0)
+           printf("\n Error. No record found.");
+        
     }
     if (choice == 2)
     {
         printf("Enter the first name: \n");
         scanf("%s", name);
-        printf("\nSearch Result.\n\n");
-        for (i = 0; i < *numberOfAccounts; i++)
+        for (i = 0; i < *num_acc; i++)
         {
-            if (strcmp(account[i].name, name) == 0)
+            if (strcmp(acc[i].name, name) == 0)
             {
-                printf("Given Account No. is registered with following details\n");
-                printf("Account No.:%d\tName:%s\tBalance:%f\tAccount Type:%s\n", account[i].account_no, account[i].name, account[i].balance, account[i].account_type);
-                searchflag = 1;
+                printf("\n The details are as follows");
+                printf("\n\tAccount No.:%d\n\tName:%s\n\tAmount:%f\n\tAccount Type:%s", acc[i].acc_no, acc[i].name, acc[i].amt, acc[i].acc_type);
+                flag = 1;
                 return SUCCESS;
             }
         }
-        if (searchflag == 0)
-        {
-            printf("Error. No record found. \n");
-        }
+        if (flag == 0)        
+            printf("\n Error. No record found.");
+        
     }
 }
