1ac1cc72a2c48569db666b4aa899c9610b208914
ad-6 <80580304+ad-6@users.noreply.github.com>
2021-04-16 10:47:35 +0530

    Update bank_wf.c

--- 3_Implementation/src/bank_wf.c
+++ 3_Implementation/src/bank_wf.c
@@ -3,19 +3,19 @@
 #include<string.h>
 #include "bankhead.h"
 
-error_t write_file(acc_info *account, int *numberOfAccounts, char *filename)
+error_t write_file(acc_info *acc, int *num_acc, char *filename)
 {
     int i;
 	FILE *fptr;
 	fptr = fopen(filename,"w");
 	if(fptr == NULL)
 	{
-		printf("No record found.\n");
+		printf("\n No record found.");
 		exit(1);
 	}
-	for(i=0;i<*numberOfAccounts;i++)
+	for(i=0;i<*num_acc;i++)
 	{
-		fwrite(&account[i],sizeof(account[i]),1,fptr);
+		fwrite(&acc[i],sizeof(acc[i]),1,fptr);
 		return SUCCESS;
 		fclose(fptr);
 	}
