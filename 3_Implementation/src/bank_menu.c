7080694d7eb1d4ba97f5471b916c16482ebd75bd
ad-6 <80580304+ad-6@users.noreply.github.com>
2021-04-16 10:51:25 +0530

    Update bank_menu.c

--- 3_Implementation/src/bank_menu.c
+++ 3_Implementation/src/bank_menu.c
@@ -9,7 +9,7 @@
  * 
  */
 
-void menu(acc_info *account, int *numberOfAccounts, char *filename)
+void menu(acc_info *acc, int *num_acc, char *filename)
 {
     int choice=0;
     system("cls");
@@ -20,18 +20,17 @@ void menu(acc_info *account, int *numberOfAccounts, char *filename)
         printf("\n\n\t\t1.Create a new account\n\t\t2.Check the details of existing account\n\t\t3.Exit\n\n\n\n\n\t\t Enter your choice:");
         scanf("%d",&choice);
         if(choice == 1)
-        {
-            new_acc(account,numberOfAccounts);
-        }
-        if(choice == 2)
-        {
-            view_acc(account,numberOfAccounts);
-        }
+            new_acc(acc,num_acc);
+        
+        if(choice == 2)        
+            view_acc(acc,num_acc);
+        
         if(choice == 3)
         {
-            write_file(account,numberOfAccounts,filename);
+            write_into_file(acc,num_acc,filename);
             printf("\n Thank you. You have exited successfully.");
             break;
         }
      }
+    return 0;
 }
